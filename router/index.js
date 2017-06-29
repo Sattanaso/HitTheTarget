7575bfe9a6bd871b95fc8ce98dc1cfb188b8ceff
Sattanaso <webdjsandpy@gmail.com>
2017-06-29 11:38:40 +0300

    logo

--- router/index.js
+++ router/index.js
@@ -4,6 +4,9 @@ const controller = require("../controllers")(data);
 module.exports = (app) => {
     app.get("/home", controller.main.home);
     app.get("/sharing", controller.main.sharing);
+    app.get("/change-password", controller.user.changePassword);
+    app.get("/contact-us", controller.user.contactUs);
+    app.get("/sign-in", controller.user.signIn);
 
     app.get("*", (req, res) => {
         res.redirect("/home");
